a13a108ed25d8f691fbe3689309f4fc93345f7a5
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2019-09-25 12:36:25 +0300

    Update IsTheBitSet.cpp

--- IsTheBitSet.cpp
+++ IsTheBitSet.cpp
@@ -4,27 +4,25 @@
 int main() {
 
   unsigned long long number;
-  const unsigned long NUMMAX = std::numeric_limits<unsigned long>::max();
-  const unsigned long NUMMIN = 1;
-  unsigned long bitOne = NUMMIN;
-  std::cout <<"Program Is the bit set"<<
-  "Enter the number from"<< NUMMIN <<" to "<< NUMMAX<<"\n";
+  const unsigned long NUM_MAX = std::numeric_limits<unsigned long>::max();
+  const unsigned long NUM_MIN = 1;
+  std::cout << "Program Is the bit set"
+            << "Enter the number from" << NUM_MIN << " to " << NUM_MAX << "\n";
   std::cin >> number;
-  if (number >= NUMMIN && number <= NUMMAX) {
-    const short BITMAX = 32;
-    const short BITMIN = 1;
-    std::cout << "Enter the bit from" << BITMIN << " to " << BITMAX << "\n";
+  if (number >= NUM_MIN && number <= NUM_MAX) {
+    const short BIT_MAX = 32;
+    const short BIT_MIN = 1;
+    std::cout << "Enter the bit from " << BIT_MIN << " to " << BIT_MAX << "\n";
     unsigned short bit;
     std::cin >> bit;
-    if (bit >= BITMIN && bit <= BITMAX) {
-      bitOne <<= (bit - 1);
-	    (number & bitOne) == bitOne ? std::cout <<"yes\n" : std::cout <<"no\n";
+    if (bit >= BIT_MIN && bit <= BIT_MAX) {
+      unsigned long bit_One = NUM_MIN;
+      bit_One <<= (bit - 1);
+      std::cout << ((number & bit_One) == bit_One) ? "yes\n" : "no\n";
+    } else {
+      std::cout << "Bit should be from " << BIT_MIN << " to " << BIT_MAX;
     }
-    else {
-      std::cout << "Bit should be from "<< BITMIN <<" to "<< BITMAX;
-    }
-  }
-  else {
+  } else {
     std::cout << "Wery long number or negative number or number = 0";
   }
   return 0;
