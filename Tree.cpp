fa342dc443527c9b1720e66defde840ca099dfcc
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2019-09-25 11:27:50 +0300

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -4,30 +4,29 @@ int main() {
   const unsigned short MAX = 100;
   const unsigned short MIN = 3;
   std::cout << "Program for drawing Tree \n"
-  "please enter number from " 
-  << MIN << " to " << MAX << "\n";
+               "please enter number from "
+            << MIN << " to " << MAX << "\n";
   unsigned short count;
   std::cin >> count;
   if (count >= MIN && count <= MAX) {
     unsigned short inHalf = count / 2;
-    short start = inHalf;
+    unsigned short start = inHalf;
     unsigned short end = inHalf;
     while (start >= 0) {
-      for (int i = 0; i < start; ++i) {
+      for (unsigned short i = 0; i < start; ++i) {
         std::cout << ' ';
       }
-      for (int i = start; i <= end; ++i) {
+      for (unsigned short i = start; i <= end; ++i) {
         std::cout << '*';
       }
-      for (int i = end; i < count; ++i) {
+      for (unsigned short i = end; i < count; ++i) {
         std::cout << ' ';
       }
       --start;
       ++end;
       std::cout << '\n';
     }
-  }
-  else {
+  } else {
     std::cout << "Range from " << MIN << " to " << MAX;
   }
   return 0;
