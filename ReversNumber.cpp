5f72969cf43ea6dfb48ebf2a8c1108e5b2b3cedc
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2019-09-25 09:58:38 +0300

    Update ReversNumber.cpp

--- ReversNumber.cpp
+++ ReversNumber.cpp
@@ -4,23 +4,22 @@
 int main() {
 
   long long num;
-  constexpr long MAX = std::numeric_limits<long>::max();
-  constexpr long MIN = std::numeric_limits<long>::min();
+  constexpr unsigned int MAX = std::numeric_limits<unsigned int>::max();
+  constexpr unsigned int MIN = std::numeric_limits<unsigned int>::min();
   std::cout << "Program for reverse numbers\n";
-  std::cout << "Enter number from "<< MIN <<" to "<< MAX <<"\n";
+  std::cout << "Enter number from " << MIN << " to " << MAX << "\n";
   std::cin >> num;
   if (num >= MIN && num <= MAX) {
-    long rev = 0;
-    while (num != 0){
-    long count;
-    count = num % 10;
-    rev = rev * 10 + count;
-    num = num / 10;
+    unsigned int rev = 0;
+    while (num != 0) {
+      auto count = num % 10;
+      rev = rev * 10 + count;
+      num = num / 10;
     }
     std::cout << "Reverse = " << rev << "\n";
-  }
-  else {
-    std::cout << "number should beat minimal = " << MIN << " and maximal = " << MAX << "\n";
+  } else {
+    std::cout << "number should beat minimal = " << MIN
+              << " and maximal = " << MAX << "\n";
   }
   return 0;
 }
