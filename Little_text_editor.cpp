f6f9913d82f1b162dac2f19872fc9df9d9faa2dd
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2019-10-01 12:32:44 +0300

    Update Little_text_editor.cpp

--- Little_text_editor.cpp
+++ Little_text_editor.cpp
@@ -2,91 +2,97 @@
 
 enum { DELTA = 32 };
 
-void Find_Diget(char *arg_symbol, size_t arg_length) {
-  if (nullptr == arg_symbol) {
+void Find_Diget(char *begin) {
+  if (nullptr == begin && *begin == '\0') {
     return;
   }
   bool is_digit_present = false;
-  for (size_t i = 0; i < arg_length; ++i) {
-    if (arg_symbol[i] >= '0' && arg_symbol[i] <= '9') {
+  while (*begin != '\0') {
+    if (*begin >= '0' && *begin <= '9') {
       is_digit_present = true;
       break;
     }
+    ++begin;
   }
   if (is_digit_present) {
     std::cout << "Is Digit = ";
-    for (size_t i = 0; i < arg_length; ++i) {
-      auto ch = arg_symbol[i];
+    while (*begin != '\0') {
+      auto ch = *begin;
       if (ch >= '0' && ch <= '9') {
         std::cout << ch;
       }
+      ++begin;
     }
   } else {
     std::cout << "No digits in string \n";
   }
 }
 
-void To_lower(char *arg_symbol, size_t arg_length) {
-  if (nullptr == arg_symbol) {
+void To_Upper(char *begin) {
+  if (nullptr == begin && *begin == '\0') {
     return;
   }
-  for (size_t i = 0; i <= arg_length; ++i) {
-    if (arg_symbol[i] >= 'a' && arg_symbol[i] <= 'z') {
-      size_t temp = arg_symbol[i] - DELTA;
+  while (*begin != '\0') {
+    if (*begin >= 'a' && *begin <= 'z') {
+      auto temp = *begin - DELTA;
       std::cout << (char)temp;
     } else {
-      std::cout << arg_symbol[i];
+      std::cout << *begin;
     }
+    ++begin;
   }
 }
 
-void To_Upper(char *arg_symbol, size_t arg_length) {
-  if (nullptr == arg_symbol) {
+void To_lower(char *begin) {
+  if (nullptr == begin && *begin == '\0') {
     return;
   }
-  for (size_t i = 0; i <= arg_length; ++i) {
-    if (arg_symbol[i] >= 'A' && arg_symbol[i] <= 'Z') {
-      size_t temp = arg_symbol[i] + DELTA;
+  while (*begin != '\0') {
+    if (*begin >= 'A' && *begin <= 'Z') {
+      auto temp = *begin + DELTA;
       std::cout << (char)temp;
     } else {
-      std::cout << arg_symbol[i];
+      std::cout << *begin;
     }
+    ++begin;
   }
 }
 
-void Revers_String(char *arg_str, size_t arg_length) {
-  if (arg_str == nullptr || arg_length == 0) {
+void Revers_String(char *begin, char *end) {
+  if (begin == nullptr && end != nullptr && begin != end) {
     return;
   }
-  for (size_t i = 0, j = arg_length - 1; i < j; ++i, --j) {
-    size_t temp = arg_str[i];
-    arg_str[i] = arg_str[j];
-    arg_str[j] = temp;
+  while (begin < end) {
+    char tmp = *begin;
+    *begin = *end;
+    *end = tmp;
+    ++begin;
+    --end;
   }
-  std::cout << arg_str << std::endl;
 }
 
 enum ChoiseFunc { FIND = 1,
-                  UPPER = 2,
-                  LOWER = 3,
-                  REVERS = 4
-};
+                 UPPER = 2,
+                 LOWER = 3,
+                 REVERS = 4
+                };
 
 int main() {
   std::cout << "In this program you can = \n"
-            << "-Find number in string \n"
+               "-Find number in string \n"
             << "-Upper case conversion \n"
             << "-Lower case conversion \n"
             << "-Revers for string \n";
   const unsigned SIZE = 255;
   char symbol[SIZE];
- 
   std::cout << "Enter STRING = ";
   std::cin >> symbol;
   size_t length = 0;
-  for (; symbol[length] != '\0'; ++length) {
+  while (symbol[++length] != '\0') {
     ;
   }
+  char *begin = symbol;
+  char *end = &symbol[length - 1];
   if (length <= SIZE) {
     std::cout << "Select a function by the following commands : \n";
     std::cout << "---1 = Find number\n"
@@ -97,16 +103,17 @@ int main() {
     std::cin >> choice;
     switch (ChoiseFunc(choice)) {
     case FIND:
-      Find_Diget(symbol, length);
+      Find_Diget(symbol);
       break;
     case UPPER:
-      To_lower(symbol, length);
+      To_Upper(symbol);
       break;
     case LOWER:
-      To_Upper(symbol, length);
+      To_lower(symbol);
       break;
     case REVERS:
-      Revers_String(symbol, length);
+      Revers_String(symbol, end);
+      std::cout << symbol;
       break;
     default:
       std::cout << "UNDEFINED CHOICE";
