91c034be9538aba38e215d6eef2b8b3f3ace3b1d
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2019-10-26 14:26:02 +0300

    Update Sort_Of_Compare.cpp

--- Sort_Of_Compare.cpp
+++ Sort_Of_Compare.cpp
@@ -16,128 +16,108 @@ void PrintArray(int *begin, int *end) {
     return;
   }
   while (begin != end) {
-    std::cout << *begin++ << " ";
+    std::cout << *begin++ << std::endl;
   }
 }
 
-typedef bool (*Compare)(int, int);
-
-void Bubble_Sort(int *begin, int *end, Compare cmp) {
+void Bubble_Sort(int *begin, int *end, std::function<bool(int, int)> cmp) {
   if (nullptr == begin || nullptr == end || begin == end || begin == 0) {
     return;
   }
   if (nullptr == begin || nullptr == end || begin == end) {
     return;
   }
-  for (--end; begin < end; ++begin) {
-    for (auto *end_copy = end; end_copy > begin; end_copy--) {
-      if (cmp(*end_copy, *begin)) {
-        int tmp = *end_copy;
-        *end_copy = *begin;
-        *begin = tmp;
+  for (auto it = begin; it != end; ++it) {
+    for (auto it_next = (it + 1); it_next != end; ++it_next) {
+      if (cmp(*it, *it_next)) {
+        auto tmp = *it;
+        *it = *it_next;
+        *it_next = tmp;
       }
     }
   }
 }
 
-void Quick_Sort(int *numbers, int left, int right) {
-  int pivot;
-  int l_hold = left;
-  int r_hold = right;
-  pivot = numbers[left];
-  while (left < right) {
-    while ((numbers[right] >= pivot) && (left < right))
-      right--;
-    if (left != right) {
-      numbers[left] = numbers[right];
-      left++;
+void Quick_Sort(int *arr, int left, int right) {
+  int l = left;
+  int r = right - 1;
+  int size = right - left;
+  if (size > 1) {
+    int pivot = arr[rand() % size + l];
+    while (l < r) {
+      while (arr[r] > pivot && r > l) {
+        r--;
+      }
+      while (arr[l] < pivot && l <= r) {
+        l++;
+      }
+      if (l < r) {
+        auto tmp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = tmp;
+        l++;
+      }
     }
-    while ((numbers[left] <= pivot) && (left < right))
-      left++;
-    if (left != right) {
-      numbers[right] = numbers[left];
-      right--;
-    }
+    Quick_Sort(arr, left, l);
+    Quick_Sort(arr, r, right);
   }
-  numbers[left] = pivot;
-  pivot = left;
-  left = l_hold;
-  right = r_hold;
-  if (left < pivot)
-    Quick_Sort(numbers, left, pivot - 1);
-  if (right > pivot)
-    Quick_Sort(numbers, pivot + 1, right);
 }
 
-enum { LESS = 1,
-       MORE
-     };
+enum { LESS = 1, MORE };
 
 int main() {
 
-  srand(static_cast<unsigned int>(time(0)));
+  srand(<unsigned int>(time(0)));
   int size;
-  std::cout << "This program is for comparing a bubble sort and a Quick sort using a comparator \n "
-            <<"please enter the size of the items to sort \n "
+  std::cout << "This program is for comparing a bubble sort and a Quick sort "
+               "using a comparator \n "
+            << "please enter the size of the items to sort \n ";
   std::cin >> size;
   if (size <= 0) {
     std::cout << "Invalid size";
     return 1;
   }
-  int *bubble_arr = new int[size];
-  int *begin = bubble_arr;
-  int *end = bubble_arr + size;
+  int *array = new int[size];
 
-  int *quick_arr = new int[size];
-  int *begin_quick = quick_arr;
-  int *end_quick = quick_arr + size;
-
-  auto less = [](int lh, int rh) { return lh < rh; };
-  auto more = [](int lh, int rh) { return lh > rh; };
   std::chrono::time_point<std::chrono::system_clock> start_Bubble_sort,
       end_Bubble_sotr;
   std::chrono::time_point<std::chrono::system_clock> start_Quick_sort,
       end_Quick_sotr;
   std::chrono::seconds;
-
-  FillRandomArray(begin, end);
-
+  FillRandomArray(array, array + size);
   std::cout << "Please input Less = 1 or More = 2 \n";
   unsigned short choise;
   std::cin >> choise;
 
   if (choise == LESS) {
     start_Bubble_sort = std::chrono::system_clock::now();
-    Bubble_Sort(begin, end, less);
+    Bubble_Sort(array, array + size, [](int lh, int rh) { return lh < rh; });
     end_Bubble_sotr = std::chrono::system_clock::now();
   } else if (choise == MORE) {
     start_Bubble_sort = std::chrono::system_clock::now();
-    Bubble_Sort(begin, end, more);
+    Bubble_Sort(array, array + size, [](int lh, int rh) { return lh > rh; });
     end_Bubble_sotr = std::chrono::system_clock::now();
   } else {
     std::cout << "Sort is not imposible";
-    return 1;
+    return 0;
   }
-  PrintArray(begin, end);
+  PrintArray(array, array + size);
   std::chrono::duration<double> elapsed_seconds_Bubble =
       end_Bubble_sotr - start_Bubble_sort;
 
   std::cout << "-----------------------\n";
 
-  FillRandomArray(begin_quick, end_quick);
+  FillRandomArray(array, array + size);
   start_Quick_sort = std::chrono::system_clock::now();
-  Quick_Sort(quick_arr, 0, size - 1);
+  Quick_Sort(array, 0, size - 1);
   end_Quick_sotr = std::chrono::system_clock::now();
-  PrintArray(begin_quick, end_quick);
+  PrintArray(array, array + size);
   std::chrono::duration<double> elapsed_seconds_Quick =
       end_Quick_sotr - start_Quick_sort;
-
   std::cout << "\n\n"
             << "Bubble sort time = " << elapsed_seconds_Bubble.count() << "\n";
-
   std::cout << "\n\n"
             << "Quick sort time = " << elapsed_seconds_Quick.count() << "\n";
-  delete [] bubble_arr;
-  delete [] quick_arr;
+  delete[] array;
   return 0;
 }
