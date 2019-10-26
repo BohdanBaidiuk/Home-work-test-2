2c4fb2a9ba88a75467d415c857025313336b4a71
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2019-10-26 16:52:32 +0300

    Update Sorting_By_Different_Type.cpp

--- Sorting_By_Different_Type.cpp
+++ Sorting_By_Different_Type.cpp
@@ -4,49 +4,40 @@ enum CHOISE { UNSIGNED_TYPE = 1, DOUBLE_TYPE = 2, CHAR_TYPE };
 
 enum { LESS = 1, MORE };
 
-typedef bool (*Compare)(unsigned, unsigned);
-void Bubble_sort(unsigned *begin, unsigned *end, Compare cmp) {
+void Bubble_sort(unsigned *begin, unsigned *end, std::function<bool(int, int)> cmp) {
   if (nullptr == begin || nullptr == end || begin == end || begin == 0) {
     return;
   }
-  for (--end; begin < end; ++begin) {
-    for (unsigned *end_copy = end; end_copy > begin; end_copy--) {
-      if (cmp(*end_copy, *begin)) {
-        unsigned tmp = *end_copy;
-        *end_copy = *begin;
-        *begin = tmp;
+  for (auto it = begin; it != end; ++it) {
+    for (auto it_next = (it + 1); it_next != end; ++it_next) {
+      if (cmp(*it, *it_next)) {
+        std::swap(*it, *it_next);
       }
     }
   }
 }
 
-typedef bool (*CompareD)(double, double);
-void Bubble_sort(double *begin, double *end, CompareD cmp) {
+void Bubble_sort(double *begin, double *end, std::function<bool(int, int)> cmp) {
   if (nullptr == begin || nullptr == end || begin == end || begin == 0) {
     return;
   }
-  for (--end; begin < end; ++begin) {
-    for (double *end_copy = end; end_copy > begin; end_copy--) {
-      if (cmp(*end_copy, *begin)) {
-        double tmp = *end_copy;
-        *end_copy = *begin;
-        *begin = tmp;
+  for (auto it = begin; it != end; ++it) {
+    for (auto it_next = (it + 1); it_next != end; ++it_next) {
+      if (cmp(*it, *it_next)) {
+       std::swap(*it, *it_next);
       }
     }
   }
 }
 
-typedef bool (*CompareCh)(char, char);
-void Bubble_sort(char *begin, char *end, CompareCh cmp) {
+void Bubble_sort(char *begin, char *end, std::function<bool(int, int)> cmp) {
   if (nullptr == begin || nullptr == end || begin == end || begin == 0) {
     return;
   }
-  for (--end; begin < end; ++begin) {
-    for (char *end_copy = end; end_copy > begin; end_copy--) {
-      if (cmp(*end_copy, *begin)) {
-        char tmp = *end_copy;
-        *end_copy = *begin;
-        *begin = tmp;
+  for (auto it = begin; it != end; ++it) {
+    for (auto it_next = (it + 1); it_next != end; ++it_next) {
+      if (cmp(*it, *it_next)) {
+       std::swap(*it, *it_next);
       }
     }
   }
@@ -56,7 +47,7 @@ void Fill_Array(unsigned *begin, unsigned *end) {
   if (nullptr == begin || nullptr == end || begin == end) {
     return;
   }
-  unsigned count = 0;
+  size_t count = 0;
   while (begin != end) {
     std::cout << ++count << " = ";
     std::cin >> *begin++;
@@ -67,7 +58,7 @@ void Fill_Array(double *begin, double *end) {
   if (nullptr == begin || nullptr == end || begin == end) {
     return;
   }
-  unsigned count = 0;
+  size_t count = 0;
   while (begin != end) {
     std::cout << ++count << " = ";
     std::cin >> *begin++;
@@ -78,7 +69,7 @@ void Fill_Array(char *begin, char *end) {
   if (nullptr == begin || nullptr == end || begin == end) {
     return;
   }
-  unsigned count = 0;
+  size_t count = 0;
   while (begin != end) {
     std::cout << ++count << " = ";
     std::cin >> *begin++;
@@ -120,14 +111,6 @@ int main() {
   unsigned *arr_Un = new unsigned[size];
   double *arr_D = new double[size];
   char *arr_Ch = new char[size];
-  unsigned *begin_Un = arr_Un;
-  unsigned *end_Un = arr_Un + size;
-  double *begin_D = arr_D;
-  double *end_D = arr_D + size;
-  char *begin_Ch = arr_Ch;
-  char *end_Ch = arr_Ch + size;
-  auto less = [](auto lh, auto rh) { return lh < rh; };
-  auto more = [](auto lh, auto rh) { return lh > rh; };
   int choise, skew;
   std::cout << "Please select the type of data to sort = \n"
             << "1 - Unsigned type \n"
@@ -137,43 +120,32 @@ int main() {
   std::cout << "Select sorting from large or from smaller \n"
             << "1 - from smaller \n"
             << "2 - from large \n";
-  do {
-    std::cin >> skew;
-    if (skew == LESS) {
-      break;
-    } else if (skew == MORE) {
-      break;
-    }
-    std::cout << "Tray agein \n";
-  } while (true);
+  auto less = [](auto lh, auto rh) { return lh < rh; };
+  auto more = [](auto lh, auto rh) { return lh > rh; };
+  std::cin >> skew;
+  if (skew == LESS) {
+    break;
+  } else if (skew == MORE) {
+    break;
+  } else {
+    return 0;
+  }
   switch (CHOISE(choise)) {
   case UNSIGNED_TYPE:
-    Fill_Array(begin_Un, end_Un);
-    if (skew == LESS) {
-      Bubble_sort(begin_Un, end_Un, less);
-    } else if (skew == MORE) {
-      Bubble_sort(begin_Un, end_Un, more);
-    }
-    Print_Array(begin_Un, end_Un);
+    Fill_Array(arr_Un, arr_Un + size);
+    Bubble_sort(arr_Un, arr_Un + size, skew == LESS ? less : more);
+    Print_Array(arr_Un, arr_Un + size);
     break;
 
   case DOUBLE_TYPE:
-    Fill_Array(begin_D, end_D);
-    if (skew == LESS) {
-      Bubble_sort(begin_D, end_D, less);
-    } else if (skew == MORE) {
-      Bubble_sort(begin_D, end_D, more);
-    }
-    Print_Array(begin_D, end_D);
+    Fill_Array(arr_D, arr_D + size);
+    Bubble_sort(arr_D, arr_D + size, skew == LESS ? less : more);
+    Print_Array(arr_D, arr_D + size);
     break;
   case CHAR_TYPE:
-    Fill_Array(begin_Ch, end_Ch);
-    if (skew == LESS) {
-      Bubble_sort(begin_Ch, end_Ch, less);
-    } else if (skew == MORE) {
-      Bubble_sort(begin_Ch, end_Ch, more);
-    }
-    Print_Array(begin_Ch, end_Ch);
+    Fill_Array(arr_Ch, arr_Ch + size);
+    Bubble_sort(arr_Ch, arr_Ch + size, skew == LESS ? less : more);
+    Print_Array(arr_Ch, arr_Ch + size);
 
     break;
   default:
